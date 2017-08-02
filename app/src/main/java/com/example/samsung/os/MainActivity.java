1edae70510098e1f71cadb05961666945eb2538f
JWY <linkclean@naver.com>
2017-08-02 17:27:20 +0900

    test2

--- app/src/main/java/com/example/samsung/os/MainActivity.java
+++ app/src/main/java/com/example/samsung/os/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView)findViewById(R.id.phoneNumber);
+        //앙 호형띠~
     }
 }
